5d6178db07b5dea0d87342306310b93697099ff8
touyu <akkey0222@gmail.com>
2015-11-29 14:21:58 +0900

    Initial Commit

---
+++ 0ch-Scraping-AppTests/_ch_Scraping_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  _ch_Scraping_AppTests.swift
+//  0ch-Scraping-AppTests
+//
+//  Created by 秋葉祐人 on 2015/11/29.
+//  Copyright © 2015年 秋葉祐人. All rights reserved.
+//
+
+import XCTest
+@testable import _ch_Scraping_App
+
+class _ch_Scraping_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
